a1a5009424d07b3e7840f2a19927071bf58dfb78
JadenLuke <jaden_10110@hotmail.com>
2017-04-24 23:36:32 -0500

    Update signup_page.php

--- signup_page.php
+++ signup_page.php
@@ -51,7 +51,7 @@
 									title: "CBO Management",
 									text: text,
 									type:"alert-error",
-									callback: function(){if(text != "Unable To Sign Up") window.location.replace ("http://localhost:8080/webix/index.php");}
+									callback: function(){if(text != "Unable To Sign Up") window.location.replace ("https://cbo-management.herokuapp.com");}
 								});
 						
 							
